5750e1690be5fcd28a524532da3c8ddbb7d2296c
DESKTOP-NLM5CTP\unlea <sergeant.krut@gmail.com>
2023-04-21 16:02:31 +0300

    footer height

--- client/src/components/AppFooter.js
+++ client/src/components/AppFooter.js
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap';
 
 const AppFooter = () => {
 	return (
-		<Container fluid className='bg-dark text-white' style={{padding: '50px', position: 'fixed', bottom: 0, zIndex: 4}}>
+		<Container height={150} fluid className='bg-dark text-white' style={{padding: '50px', position: 'fixed', bottom: 0, zIndex: 4}}>
 			Внимание: данное веб-приложение не является рабочим интернет-магазином, оно является тестовым проектом.
 			Заказ товаров недоступен.
 		</Container>
