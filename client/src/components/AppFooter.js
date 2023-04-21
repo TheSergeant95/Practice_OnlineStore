474e3d87ac41a0f0abe7627a6615f811aedc1e3b
DESKTOP-NLM5CTP\unlea <sergeant.krut@gmail.com>
2023-04-21 17:10:31 +0300

    footer padding fix

--- client/src/components/AppFooter.js
+++ client/src/components/AppFooter.js
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap';
 
 const AppFooter = () => {
 	return (
-		<Container height={40} fluid className='bg-dark text-white' style={{padding: '50px', position: 'fixed', bottom: 0, zIndex: 4}}>
+		<Container height={40} fluid className='bg-dark text-white' style={{padding: '0 50px', position: 'fixed', bottom: 0, zIndex: 4}}>
 			Внимание: данное веб-приложение не является рабочим интернет-магазином, оно является тестовым проектом.
 			Заказ товаров недоступен.
 		</Container>
