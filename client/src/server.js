37851b2a51981bf85755711445b693acbd7044c2
DESKTOP-NLM5CTP\unlea <sergeant.krut@gmail.com>
2023-04-22 23:33:41 +0300

    server fix

--- client/src/server.js
+++ client/src/server.js
@@ -12,7 +12,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(__dirname))
 app.use(express.static(path.resolve(__dirname, 'build')))
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
